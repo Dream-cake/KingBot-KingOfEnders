d391aaca42aee483f60dfa03ec2e1186d823682c
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-19 18:57:13 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -31,14 +31,13 @@ if (king === "music"){
         .setAuthor("Hello User These Are My Commands")
         .setDescription("**`help [Command] To Show You What It Does`**")
         .setColor("#008080")
-        .setThumbnail(icon)
+//         .setThumbnail(icon)
         // .setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
         .setFooter(`${message.guild.name} | ${message.guild.memberCount} Members | Made By KingOfEnders`, `${message.guild.iconURL}`)
         .setTimestamp(new Date())
         .addField("Prefix", "**`?`**")
         .addField("Music", "**`play` <-> `stop` <-> `skip` <-> `leave` <-> `queue` <-> `np` <-> `volume`**")
         .addField("Contact", "**`Bugs/Spelling Contact Owner KingOfEnders ◢ ◤#0143`**")
-  
     message.channel.send({embed: embed});
 }
 if (king === "8ball"){
