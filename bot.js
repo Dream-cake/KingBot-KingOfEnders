263ced3b6c2845e0262807665c8faf0918e4d81b
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-20 13:30:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,25 @@ fs.readdir("./commands/", (err, files) => {
     console.log(`${f} loaded!`);
     Client.commands.set(props.help.name, props);
   });
-  
+ 
+});
+
+fs.readdir("./staffcommands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./staffcommands/${f}`);
+    console.log(`${f} loaded!`);
+    Client.commands.set(props.help.name, props);
+  });
+ 
 });
 
 // Client.on("guildMemberAdd", async member => {
