f5a8a92d13e159b0555a62772aa2d4beb5af568b
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-20 13:19:59 -0400

    Update and rename keepfolderhere to voicechannel.js

---
+++ staffcommands/voicechannel.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let icon = message.author.displayAvatarURL;
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setDescription("Your Voice Channel Settings")
+        .setColor("#5499C7")
+        .setThumbnail(icon)
+        .addField("Server Mute", message.member.serverMute)
+        .addField("Server Deafen", message.member.serverDeaf)
+        .addField("Self Mute", message.member.selfMute)
+        .addField("Self Deafen", message.member.selfDeaf)
+        .addField("Speaking", message.member.speaking)
+      
+    message.channel.send({embed: embed});
+}
+
+module.exports.help = {
+  name:"vc"
+}
