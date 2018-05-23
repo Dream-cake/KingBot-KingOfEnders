3a82148426a7895ac32612fa094377c88aadc076
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-22 22:22:05 -0400

    Update seton.js

--- botowner/seton.js
+++ botowner/seton.js
@@ -1,1 +1,15 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (Client, message, args) => {  
+var kitty = message.guild.members.find("id", "412374698987487242");
+	if (message.member !== kitty)return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` You Can't Set My Status To `Online` Only The `BotOwner` Can :warning:**");
+	if (kitty) {
+	Client.user.setStatus("online");
+	message.channel.send("**My `BotOwner` Set My Status To `Online`**");
+	console.log("You Set My Status To Online");
+	}
+}
+
+module.exports.help = {
+  name:"seton"
+}
