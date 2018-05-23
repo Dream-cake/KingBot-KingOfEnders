a2c5d6973df53690501f5e1d7a2acb961a30066a
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-22 22:33:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,23 +62,23 @@ fs.readdir("./commands/", (err, files) => {
  
 // });
 
-// fs.readdir("./help/", (err, files) => {
+fs.readdir("./help/", (err, files) => {
 
-//   if(err) console.log(err);
+  if(err) console.log(err);
 
-//   let jsfile = files.filter(f => f.split(".").pop() === "js")
-//   if(jsfile.length <= 0){
-//     console.log("Couldn't find commands.");
-//     return;
-//   }
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
 
-//   jsfile.forEach((f, i) =>{
-//     let props = require(`./help/${f}`);
-//     console.log(`${f} loaded!`);
-//     Client.commands.set(props.help.name, props);
-//   });
+  jsfile.forEach((f, i) =>{
+    let props = require(`./help/${f}`);
+    console.log(`${f} loaded!`);
+    Client.commands.set(props.help.name, props);
+  });
  
-// });
+});
 
 // fs.readdir("./botowner/", (err, files) => {
 
