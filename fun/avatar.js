7b1200d6fcc6c4cdc1482379c570a3e60c18bee4
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-22 22:39:11 -0400

    Update avatar.js

--- fun/avatar.js
+++ fun/avatar.js
@@ -1,1 +1,25 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (Client, message, args) => {
+//     let icon = message.author.displayAvatarURL;
+//     let embed = new Discord.RichEmbed()
+//     .setDescription("**This Is Your Profile Picture " + message.member.displayName + "**")
+//     .setImage(message.author.displayAvatarURL)
+//     .setColor("#BF0747");
+    let msg = await message.channel.send("**Generating Avatar...**")
+    let target = message.mentions.users.first() || message.author
+
+     message.channel.send({files: [
+        {
+           attachment: target.displayAvatarURL,
+             name: "avatar.png"
+        }
+     ]});
+
+//    message.channel.send({embed: embed});
+    msg.delete();
+}
+
+module.exports.help = {
+  name:"avatar"
+}
