533c878b7a240be164d50ca9b6810f0c64794fb1
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-22 22:43:48 -0400

    Update minecraftdeath.js

--- fun/minecraftdeath.js
+++ fun/minecraftdeath.js
@@ -1,1 +1,21 @@
+exports.run = function(client, message) {
+    message.channel.send(`${message.member.displayName} has died.`).then(Message => {
+        setTimeout(() => { Message.edit("Respawning..."); }, 1000);
+        setTimeout(() => { Message.edit(`Revival complete. Welcome back, ${message.member.displayName}`); }, 3000);
+    });
+};
 
+exports.conf = {
+    enabled: true,
+    runIn: ["text"],
+    aliases: ["kms"],
+    permLevel: 0,
+    botPerms: [],
+};
+      
+exports.help = {
+    name: "killme",
+    description: "Kill yourself with this command. Now comes with free revival!",
+    usage: "",
+    usageDelim: "",
+};
