0c879a533e2939e7dde36c04d59f398d357e8ea9
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-22 22:04:57 -0400

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` You Do Not Have Enough Perms To Mute or You Need `Manage Messages/Staff Team Role` :warning:**");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Hey, `" + message.member.displayName + "` You Do Not Have Enough Perms To Mute or You Need `Manage Messages/Staff Team Role`**");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` I Couldn't Find That Member Please Try Again :warning:**");
+  if(!rMember) return message.channel.send("**Hey, `" + message.member.displayName + "` I Couldn't Find That Member Please Try Again**");
   let role = args.join(" ").slice(22);
-  if(!role) return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` You Didn't Pick A Role Name Please Try Again :warning:**");
+  if(!role) return message.channel.send("**Hey, `" + message.member.displayName + "` You Didn't Pick A Role Name Please Try Again**");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` I Couldn't Find That Role Please Try Again :warning:**");
+  if(!gRole) return message.channel.send("**Hey, `" + message.member.displayName + "` I Couldn't Find That Role Please Try Again**");
 
-  if(rMember.roles.has(gRole.id)) return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` They Already Have That Role :warning:**");
+  if(rMember.roles.has(gRole.id)) return message.channel.send("**Hey, `" + message.member.displayName + "` They Already Have That Role**");
   await(rMember.addRole(gRole.id));
 
   try{
