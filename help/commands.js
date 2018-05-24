a7556424ab0ea40ed623875b82b2c9a0209d0e14
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-23 20:05:13 -0400

    Update commands.js

--- help/commands.js
+++ help/commands.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Fun", "**`avatar` <-> `icon` <-> `google` <-> `urban` <-> `randomUrban`<-> `rps` <-> `8ball` <-> `killme` <-> `roll` <-> `coin` <-> `meme`**")
         .addField("Special Events", "**`songs`**")
         .addField("Commands Removed", "**`warn` <-> `warnings` <-> `steve` <-> `spam` <-> `kick` <-> `ban` <-> `nameSpam` <-> `hello` <-> `good` <-> `steve`**")
-        .addField("Work-In Progress Commands", "**`warn` <-> `warnings` <-> `invites` <-> `discord` <-> `kick` <-> `ban`**")
+        .addField("Work-In Progress Commands", "**`invites` <-> `discord`**")
         .addField("Commands Being Fixed", "**`roll` <-> `rps` <-> `killme` <-> `coin` <-> `8ball`**")
         .addField("Contact", "**`Bugs/Spelling Contact Owner KingOfEnders#0143`**")
         .addField("Announcements", "**`New Looking Help Menu and Also Doesn't Send In Authors Messages (Pm/Dm It Means) \n\Updated The Bot`**")
