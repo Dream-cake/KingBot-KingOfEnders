e687903216ba74ba313f5691511ce3a9fcd05342
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-23 20:33:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,24 +8,6 @@ Client.commands = new Discord.Collection();
 
 const prefixes = require("./prefixes.json");
 
-// fs.readdir("./commands/", (err, files) => {
-
-//   if(err) console.log(err);
-
-//   let jsfile = files.filter(f => f.split(".").pop() === "js")
-//   if(jsfile.length <= 0){
-//     console.log("Couldn't find commands.");
-//     return;
-//   }
-
-//   jsfile.forEach((f, i) =>{
-//     let props = require(`./commands/${f}`);
-//     console.log(`${f} loaded!`);
-//     Client.commands.set(props.help.name, props);
-//   });
- 
-// });
-
 fs.readdir("./staff/", (err, files) => {
 
   if(err) console.log(err);
