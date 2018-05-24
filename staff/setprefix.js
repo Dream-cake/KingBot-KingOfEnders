38f34caa720387a32190538bcfa43b3bc0f0f85e
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-23 20:24:46 -0400

    Update setprefix.js

--- staff/setprefix.js
+++ staff/setprefix.js
@@ -1,42 +1,28 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
 module.exports.run = async (client,message,args) => {
-  // var OGNICK = message.guild.members.find("id", "412374698987487242");
-  // if (message.member !== OGNICK)return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` You Can't Set My NickName That's Only A `BotOwner` Command :warning:**");
-  if(!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.channel.send("**:warning: Hey, `" + message.member.displayName + "` You Do Not Have Enough Perms To Mute or You Need `View Audit Log/Staff Team Role` :warning:**");
+  if (!args[0])return message.channel.send("please enter a new prefix");
 
+  const prefixes = require("../prefixes.json");
 
-    try {
-      if (args.length > 0) {
-        await message.guild.me.setNickname(args.join(' '));
-  
-        message.channel.send({
-          embed: {
-            description: `${client.user.username}'s nick is now set to **${args.join(' ')}** on this guild.`
-          }
-        }).catch(e => {
-          console.log.error(e);
-        });
-      }
-      else {
-        await message.guild.me.setNickname('');
-  
-        message.channel.send({
-          embed: {
-            
-            description: `${client.user.username}'s nick has been reset on this guild.`
-          }
-        }).catch(e => {
-          console.log.error(e);
-        });
-      }
-    }
-    catch (e) {
-      console.log.error(e);
-    }
+  const prefix = prefixes[message.guild.id].prefixes;
+
+  if(!message.member.hasPermission("VIEW_AUDIT_LOG"))return message.channel.send("**Hey, `" + message.member.displayName + "` You Do Not Have Enough Perms To SetPrefix or You Need `View Audit Log/Staff Team` Role**");
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
   };
-  
 
-  
-  exports.help = {
-    name: 'setName'
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err)
+  });
 
-  };
+  message.delete(10000);
+  message.channel.send('New prefix set to: '+ args[0] +'').then(msg => {msg.delete(10000)}); //10000 is 10 seconds
+  console.log(`${message.member.displayName} set the prefix for ${message.guild} to: ${args[0]}`);
+
+  }
+module.exports.help = {
+  name: "setPrefix"
+}
