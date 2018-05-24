5f89edea8782a6a8d5f2aec80f3d2170ac54b070
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-23 22:25:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,12 +146,12 @@ function game4() {
 
 Client.on('message', async message => { 
 	if (message.author.bot) return undefined;
-	if (message.content.startsWith("<prefix>") && message.channel.type !== "text")return mesage.reply("Please use my commands in a server");
+	if (message.content.startsWith("?") && message.channel.type !== "text")return mesage.reply("Please use my commands in a server");
 	
 	
     if(!prefixes[message.guild.id]){
     prefixes[message.guild.id] = {
-      prefixes: "<prefix>"
+      prefixes: "?"
     };
   }
 
