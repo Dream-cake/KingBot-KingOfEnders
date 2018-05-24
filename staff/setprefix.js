189d11cfd3a580cecc7ef00d567566fdbdafd293
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-23 21:43:07 -0400

    Update setprefix.js

--- staff/setprefix.js
+++ staff/setprefix.js
@@ -1,27 +1,26 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const prefixes = require("./prefixes.json");
 
 module.exports.run = async (client,message,args) => {
-  if (!args[0])return message.channel.send("please enter a new prefix");
+const ars = message.content.split(" ").slice(1).join(" ");
+  
+  if(!ars)return message.channel.send("please enter a new prefix");
+const prefix = prefixes[message.guild.id].prefixes;
 
-  const prefixes = require("./prefixes.json");
-
-  const prefix = prefixes[message.guild.id].prefixes;
-
-  if(!message.member.hasPermission("VIEW_AUDIT_LOG"))return message.channel.send("**Hey, `" + message.member.displayName + "` You Do Not Have Enough Perms To SetPrefix or You Need `View Audit Log/Staff Team` Role**");
+  if(!message.member.hasPermission("VIEW_AUDIT_LOG"))return message.channel.send("**Hey, `"+ message.member.displayName +"` You Do Not Have Enough Perms To SetPrefix**");
 
   prefixes[message.guild.id] = {
-    prefixes: args[0]
+    prefixes: ars
   };
 
   fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
     if (err) console.log(err)
   });
 
-  message.delete(10000);
-  message.channel.send('New prefix set to: '+ args[0] +'').then(msg => {msg.delete(10000)}); //10000 is 10 seconds
-  console.log(`${message.member.displayName} set the prefix for ${message.guild} to: ${args[0]}`);
-
+  message.channel.send('New prefix set to: '+ ars +'').then(msg => {msg.delete(10000)}); //10000 is 10 seconds
+  console.log(`${message.member.displayName} set the prefix for ${message.guild} to: ${ars}`);
+message.delete(10000);
   }
 module.exports.help = {
   name: "setPrefix"
