614e631d32f8f13661439de635a4f93324abf797
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-23 20:40:36 -0400

    Update todolist.js

--- botowner/todolist.js
+++ botowner/todolist.js
@@ -1,1 +1,15 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+      if(message.author.id !== "412374698987487242") return message.channel.send("**Hey, `" + message.member.displayName + "` That Command Is Only For The BotOwner But Nice Try :wink:**")
+        message.author.send({embed:{
+                color: 0xCF8D08,
+                description: "**__TodoList__** \n\nFinish All The Error Messages \n\Add How Much Servers The Bot In \n\Fix Avatar Command \n\n**__By May 15th__**"
+            }
+        });
+
+}
+
+module.exports.help = {
+  name:"todoList"
+}
