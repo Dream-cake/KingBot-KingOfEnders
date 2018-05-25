09ba0a884b730352b000ed8af8e02275b1fb6cd3
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-24 21:38:28 -0400

    Update minecraftdeath.js

--- fun/minecraftdeath.js
+++ fun/minecraftdeath.js
@@ -1,7 +1,7 @@
 exports.run = function(client, message) {
     message.channel.send(`**${message.member.displayName} Has Fell Out Of The Minecraft World.**`).then(Message => {
 //         setTimeout(() => { Message.edit("**Respawning...**"); }, 1000);
-        setTimeout(() => { Message.edit("https://www.anime-planet.com/users/Mullen"); }, 2000);
+        setTimeout(() => { Message.edit("https://www.anime-planet.com/users/Mullen"); }, 3000);
         setTimeout(() => { Message.edit(`**Respawned Complete. Welcome Back, ${message.member.displayName}. Hope You Don't Fall Out Of The Void Again**`); }, 3000);
     });
 };
