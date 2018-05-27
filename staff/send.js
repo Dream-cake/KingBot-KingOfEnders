d682fc95cde0149a9b7d522cef7a2e0fbe049965
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-27 11:49:53 -0400

    Update send.js

--- staff/send.js
+++ staff/send.js
@@ -2,14 +2,20 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
       if(message.author.id !== "412374698987487242") return message.channel.send("**Hey, `" + message.member.displayName + "` That Command Is Only For The BotOwner *For Now Coming Out Soon For Staff* :wink:**")
-  
-mention = message.mentions.users.first();
-if (mention == null) { return; }	
-  message.delete();
-  mentionMessage = message.content.slice (8);
-  mention.send (mentionMessage);
-  message.channel.send ("**Hey, `" + message.member.displayName + "` I Send The Message To Those People You Mentioned**");
-}
+
+const sayMessage = args.join(`You have recived a message! | `);
+   if(!message.member.hasPermission("READ_MESSAGES")) return;
+     let USERM = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+     message.delete().catch();
+     USERM.send(sayMessage);
+            }
+// mention = message.mentions.users.first();
+// if (mention == null) { return; }	
+//   message.delete();
+//   mentionMessage = message.content.slice (8);
+//   mention.send (mentionMessage);
+//   message.channel.send ("**Hey, `" + message.member.displayName + "` I Send The Message To Those People You Mentioned**");
+// }
 
 module.exports.help = {
   name: "send"
