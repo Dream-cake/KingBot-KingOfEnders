9347ec69667f39da5d96e8de0d5145abafd3533d
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-27 11:53:04 -0400

    Update send.js

--- staff/send.js
+++ staff/send.js
@@ -3,10 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       if(message.author.id !== "412374698987487242") return message.channel.send("**Hey, `" + message.member.displayName + "` That Command Is Only For The BotOwner *For Now Coming Out Soon For Staff* :wink:**")
 
-const sayMessage = args.join(` You have recived a message! | `);
+const sayMessage = args.join(` **You Have Just Gotten A Message | **`);
+      message.delete();
    if(!message.member.hasPermission("READ_MESSAGES")) return;
      let USERM = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-     message.delete().catch();
+//      message.delete().catch();
      USERM.send(sayMessage);
             }
 // mention = message.mentions.users.first();
