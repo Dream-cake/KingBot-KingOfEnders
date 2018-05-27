97ce3cf0beb02a3cea53b402211e109eb57afaf9
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-27 12:07:34 -0400

    Update send.js

--- staff/send.js
+++ staff/send.js
@@ -6,9 +6,9 @@ module.exports.run = async (bot, message, args) => {
 const sayMessage = args.join(` **You Have Just Gotten A Message | **`);
       message.delete();
    if(!message.member.hasPermission("READ_MESSAGES")) return;
-     let USERM = message.guild.member(message.mentions.users.first() || message.member.displayName(args[0]));
+     let USERM = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
      USERM.send(sayMessage);
-     message.channel.send ("**Hey, `" + message.member.displayName + "` I Send The Message To The Person You Mentioned**");
+     message.channel.send ("**Hey, `" + message.member.displayName + "` I Sent The Message To The Person You Mentioned**");
 }
 // mention = message.mentions.users.first();
 // if (mention == null) { return; }	
