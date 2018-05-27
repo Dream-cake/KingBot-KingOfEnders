1e55b9a962e65ddb8e06cc3854d9c35a4e6ba345
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-27 01:14:54 -0400

    Update serverinfo.js

--- main/serverinfo.js
+++ main/serverinfo.js
@@ -11,13 +11,13 @@ module.exports.run = async (bot, message, args) => {
     .addField("Created On", message.guild.createdAt)
     .addField("You Joined", message.member.joinedAt)
     .addField("Total Members", message.guild.memberCount)
+    inline: true
     .addField("Humans", message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size)
     .addField("Bots", message.guild.members.filter(m => m.user.bot).size)
     .addField("Roles", message.guild.roles.size)
     .addField("Channels", message.guild.channels.size)
     .addField("Server Region", message.guild.region)
     .addField("Server Owner", message.guild.owner.displayName);
-    inline: true
 
     message.channel.send(serverembed);
 }
