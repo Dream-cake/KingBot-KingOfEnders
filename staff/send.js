ea2ebfa2faafd8cbab8baa48492cc34af631ca19
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-27 11:32:16 -0400

    Update send.js

--- staff/send.js
+++ staff/send.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       if(message.author.id !== "412374698987487242") return message.channel.send("**Hey, `" + message.member.displayName + "` That Command Is Only For The BotOwner *For Now Coming Out Soon For Staff* :wink:**")
   
-mention = message.mentions.users.first();
+mention = message.mention.users.first();
 if (mention == null) { return; }	
   message.delete();
   mentionMessage = message.content.slice (8);
