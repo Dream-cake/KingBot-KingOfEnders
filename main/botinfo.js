eb92b4a5e0a758bb790ea46cc3cb04bb7087c32b
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-30 22:26:05 -0400

    Update botinfo.js

--- main/botinfo.js
+++ main/botinfo.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
-const parsetime = function(milliseconds) {
-    var seconds = Math.floor(milliseconds/1000); milliseconds %= 1000;
+const parsetime = function(seconds) {
     var minutes = Math.floor(seconds/60); seconds %= 60;
     var hours = Math.floor(minutes/60); minutes %= 60;
     var days = Math.floor(hours/24); hours %= 24;
@@ -12,7 +11,6 @@ const parsetime = function(milliseconds) {
       +(minutes?(written=true,minutes+" minutes"):"")+(written?", ":"")
       +(seconds?(written=true,seconds+" seconds"):"")+(written?" ":"");
 };
-// parsetime(758678567856785678)
 
 module.exports.run = async (Client, message, args) => {
     let bicon = Client.user.displayAvatarURL;
@@ -23,7 +21,7 @@ module.exports.run = async (Client, message, args) => {
     .setFooter(`${message.guild.name} | ${message.guild.memberCount} Members | Made By KingOfEnders`, `${message.guild.iconURL}`)
     .addField("Bot Name", Client.user.username)
     .addField("Servers In", `${Client.guilds.size} Servers!`)
-    .addField('uptime:', `${parsetime(Client.uptime)}`)
+    .addField('Bot Uptime', `${parsetime(Client.uptime)}`)
     .addField("Channels Loaded", `${Client.channels.size} Channels`)
     .addField("Users Loaded", `${Client.users.size} Users`)  
     .addField("Created On", Client.user.createdAt);
