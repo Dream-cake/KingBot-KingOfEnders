ed5f81e14bbb529e249357dcb8bd8014489d0e7a
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-30 22:28:35 -0400

    Update botinfo.js

--- main/botinfo.js
+++ main/botinfo.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const parsetime = function(seconds) {
-    var minutes = Math.floor(seconds/60); seconds %= 60;
-    var hours = Math.floor(minutes/60); minutes %= 60;
-    var days = Math.floor(hours/24); hours %= 24;
-    var years = Math.floor(days/365); hours %= 24;
+    var seconds = Math.floor(seconds/60); seconds %= 60;
+    var minutes = Math.floor(minutes/60); minutes %= 60;
+    var hours = Math.floor(hours/24); hours %= 24;
+    var days = Math.floor(days/365); hours %= 24;
     var written = false;
     return(years?(written=true,years+" years"):"")+(written?", ":"")
       +(days?(written=true,days+" days"):"")+(written?", ":"")
