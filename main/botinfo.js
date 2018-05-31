0b7d46ddfe2134a9072e9856f358cc1b7ccddae2
JakubDynak <38401982+JakubDynak@users.noreply.github.com>
2018-05-30 22:14:20 -0400

    Update botinfo.js

--- main/botinfo.js
+++ main/botinfo.js
@@ -1,4 +1,18 @@
 const Discord = require("discord.js");
+const parseime = function(milliseconds) {
+    var seconds = Math.floor(milliseconds/1000); milliseconds %= 1000;
+    var minutes = Math.floor(seconds/60); seconds %= 60;
+    var hours = Math.floor(minutes/60); minutes %= 60;
+    var days = Math.floor(hours/24); hours %= 24;
+    var years = Math.floor(days/365); hours %= 24;
+    var written = false;
+    return(years?(written=true,years+" years"):"")+(written?", ":"")
+      +(days?(written=true,days+" days"):"")+(written?", ":"")
+      +(hours?(written=true,hours+" hours"):"")+(written?", ":"")
+      +(minutes?(written=true,minutes+" minutes"):"")+(written?", ":"")
+      +(seconds?(written=true,seconds+" seconds"):"")+(written?" ":"");
+};
+parsetime(758678567856785678)
 
 module.exports.run = async (Client, message, args) => {
     let bicon = Client.user.displayAvatarURL;
@@ -9,6 +23,7 @@ module.exports.run = async (Client, message, args) => {
     .setFooter(`${message.guild.name} | ${message.guild.memberCount} Members | Made By KingOfEnders`, `${message.guild.iconURL}`)
     .addField("Bot Name", Client.user.username)
     .addField("Servers In", `${Client.guilds.size} Servers!`)
+    .addField('uptime:', `${parseTime(Client.uptime)}`)
     .addField("Channels Loaded", `${Client.channels.size} Channels`)
     .addField("Users Loaded", `${Client.users.size} Users`)  
     .addField("Created On", Client.user.createdAt);
